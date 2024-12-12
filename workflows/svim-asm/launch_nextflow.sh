f7cb521f319e3d401cff411ec6c4e1de1361b46b
Wen-Wei Liao <wen-wei.liao@yale.edu>
2024-12-11 22:54:19 -0500

    Add Slurm script for launching SVIM-asm workflow with Nextflow

---
+++ workflows/svim-asm/launch_nextflow.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=launch_nextflow
+#SBATCH --output=launch_nextflow-%j.out
+#SBATCH --partition=pi_hall
+#SBATCH --constraint=nogpu
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=5G
+#SBATCH --time=28-00:00:00
+
+module purge
+module load Nextflow/24.04.4
+
+SAMPLE_SHEET="samplesheet.csv"
+REF_FASTA="GRCh38_no_alt.fa"
+REF_KMERS="repetitive_k19.txt"
+OUTDIR="results"
+
+nextflow run main.nf \
+	-ansi-log false \
+	-profile mccleary \
+    --sample_sheet ${SAMPLE_SHEET} \
+	--ref_fasta ${REF_FASTA} \
+	--ref_kmers ${REF_KMERS} \
+	--outdir ${OUTDIR}
