c7bb2231bded79320ecf34784525e468264253c6
Wen-Wei Liao <wen-wei.liao@yale.edu>
2024-12-15 23:02:10 -0500

    Add workflow for aligning PacBio HiFi reads (FASTQ/BAM) to a reference genome using Winnowmap

---
+++ workflows/winnowmap_hifi/launch_winnowmap_hifi.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=launch_winnowmap_hifi
+#SBATCH --output=launch_winnowmap_hifi-%j.out
+#SBATCH --partition=pi_hall
+#SBATCH --constraint=nogpu
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=5G
+#SBATCH --time=28-00:00:00
+
+module purge
+module load Nextflow/24.04.4
+
+SAMPLE_SHEET="samplesheet.csv"
+REF_FASTA="GRCh38_no_alt.fa"
+REF_KMERS="repetitive_k15.txt"
+OUTDIR="results"
+
+nextflow run main.nf \
+    -ansi-log false \
+    -profile mccleary \
+    --sample_sheet ${SAMPLE_SHEET} \
+    --ref_fasta ${REF_FASTA} \
+    --ref_kmers ${REF_KMERS} \
+    --outdir ${OUTDIR}
